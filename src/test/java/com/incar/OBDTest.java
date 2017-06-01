d9cb99d6820d6ff9f7baa7703d69483496ca172a
zhouyongbo <1163903223@qq.com>
2017-06-01 17:26:41 +0800

    测试修改

--- src/test/java/com/incar/OBDTest.java
+++ src/test/java/com/incar/OBDTest.java
@@ -2,15 +2,12 @@ package com.incar;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by zhouyongbo on 2017/5/31.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = Application.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = Application.class)
 public class OBDTest {
 
     private static final Logger logger = Logger.getLogger(OBDTest.class);
